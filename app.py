03e557f3c87255014740e82e8cc4115aca35eb13
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-21 16:02:15 +0530

    2022/9/21 -7

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import *
 
+
+ALLOWED_EXTENTIONS = {'txt', 'pdf', 'docs'}
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload():
+  return render_template('index.html')
+
+@app.route('/success', methods=['GET','POST'])
+def success():
+  if request.method == 'POST':
+    file = request.files['file']
+    file.save(file.filename)
+    return render_template('success.html', name=file.filename)
