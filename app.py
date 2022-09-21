606da24e988e69eacaf660e8da4afbe130cf9e8e
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-21 16:29:54 +0530

    2022/9/21 - 8

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import *
+import os
+from werkzeug.utils import secure_filename
 
 
+UPLOAD_FOLDER = '/uploaded files'
 ALLOWED_EXTENTIONS = {'txt', 'pdf', 'docs'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def upload():
   return render_template('index.html')
 
+# allowed files checking function
+def allowed_file(filename):
+  return '.' in filename and \
+    filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENTIONS
+
+
 @app.route('/success', methods=['GET','POST'])
 def success():
   if request.method == 'POST':
     file = request.files['file']
-    file.save(file.filename)
-    return render_template('success.html', name=file.filename)
+    if not allowed_file(file.filename):
+      flash('only .txt, .pdf, .docs files allowed !!!')
+    else:
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+      return render_template('success.html', name=file.filename)
