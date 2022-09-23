0d970d3721c654145690383aa519ced13ec699fd
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-23 20:24:06 +0530

    2022/9/23 - 4

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-app.config['UPLOAD_EXTENSIONS'] = ['.txt','.docs',',docx','.pdf']
-app.config['UPLOAD_PATH'] = '/statics/uploaded files'
+app.config['UPLOAD_FOLDER'] = '/static/uploaded files'
+app.config['ALLOWED_EXTENSIONS'] = ['.txt','.pdf','docs','docx']
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
 @app.route('/success', methods=['GET','POST'])
-def upload_files():
-  uploaded_file = request.files['file']
-  filename = secure_filename(uploaded_file.filename)
-  if filename != '':
-    file_ext = os.path.splitext(filename)[1]
-    if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-      abort(400)
-    uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-  return render_template('success.html',name=uploaded_file)
+def upload_file():
+  if request.method == 'POST':
+    file = request.files['file']
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
+  return render_template('success.html', name=file.filename)  
