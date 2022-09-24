d32022a160bdac1b9347087f0d413cadda0a99f2
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-24 16:28:44 +0530

    2022/9/24 -1

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from werkzeug.utils import secure_filename
-import os
 
-app = Flask(__name__)
-
-app.config['UPLOAD_FOLDER'] = '/static/uploaded files'
-app.config['ALLOWED_EXTENSIONS'] = ['.txt','.pdf','docs','docx']
-
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/success', methods=['GET','POST'])
-def upload_file():
-  if request.method == 'POST':
-    file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-  return render_template('success.html', name=file.filename)  
