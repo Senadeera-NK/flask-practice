b16c733502eaf3171660b89677b6aabc43e613d4
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-25 11:34:24 +0530

    2022/9/25

--- app.py
+++ app.py
@@ -1,30 +1,15 @@
-from flask import Flask, flash, request, redirect, render_template
-from werkzeug.utils import secure_filename
-import os
-
-UPLOAD_FOLDER = 'static/files'
-ALLOWED_EXTENSIONS = {'.txt', '.pdf', '.docs', '.docx'}
+from pickletools import read_uint1
+from flask import *
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def index():
+def upload():
   return render_template('index.html')
 
-@app.route('/success', methods=['GET','POST'])
-def upload_files():
+@app.route('/success', methods=['POST'])
+def success():
   if request.method == 'POST':
-    if 'file' not in request.files:
-      flash('no file part')
-    file = request.files['file']
-    if file.filename == '':
-      flash('no selected files')
-    if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-  return render_template('success.html', name=filename)
+    f = request.files['file']
+    f.save(f.filename)
+    return render_template('success.html', name=f.filename)
