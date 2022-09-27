51547cf75409adaa222305eaf1f2730fe32d2fa3
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-27 15:03:18 +0530

    2022/9/27 - 4

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import *
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'uploads/files'
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def upload():
-    return render_template('index.html')
+    return render_template('upload.html')
 
-@app.route('/success', methods = ['POST'])
+@app.route('/success', methods=['GET','POST'])
 def success():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)
-        return render_template("success.html", name = f.filename)
+        filename = secure_filename (f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template('success.html', name=f.filename)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
