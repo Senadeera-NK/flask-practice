8eb1657458386948e2c9d644d89897a3bab71bac
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-27 11:52:32 +0530

    2022/9/27

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def upload():
-  return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/success', methods=['POST'])
+@app.route('/success', methods = ['POST'])
 def success():
-  if request.method == 'POST':
-    f = request.files['file']
-    f.save(f.filename)
-    return render_template('success.html', name=f.filename)
-  
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return render_template("success.html", name = f.filename)
+
+if __name__ == '__main__':
+    app.run(debug = True)
