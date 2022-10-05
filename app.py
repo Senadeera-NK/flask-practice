68634ac1859816eeec39990f932ef969e8461ffc
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-10-05 21:45:44 +0530

    2022/10/05

--- app.py
+++ app.py
@@ -1,27 +1,46 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+import os      # For File Manipulations like get paths,rename
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# app connecting to db
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-  id  = db.Column(db.Integer, primary_key=True)
-  content = db.Column(db.String(200), nullable=False)
-  data_created = db.Column(db.DateTime, default=datetime.utcnow)
-  
-  # return string everytime creating a new element
-  def __replr__(self):
-    return '<Task %r>' % self.id
-  
+app=Flask(__name__)
+app.secret_key = "secret key" # for encrypting the session
+#It will allow below 16MB contents only, you can change it
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+path = os.getcwd()
+# file Upload
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+# Make directory if "uploads" folder not exists
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def upload_form():
+    return render_template('upload.html')
 
 
-if __name__ == '__main__':
-  # upadte instantely
-  app.run(debug=True)
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        files = request.files.getlist('files[]')
+
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        flash('File(s) successfully uploaded')
+        return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
